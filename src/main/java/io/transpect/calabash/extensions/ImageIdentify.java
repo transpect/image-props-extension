8a54ae4ae98d5d780b619763a899151ec42457a7
Martin Kraetke <mkraetke@gmx.de>
2019-11-07 13:04:08 +0100

    update commons-imaging to alpha-1, more robust colorspace evaluation

--- src/main/java/io/transpect/calabash/extensions/ImageIdentify.java
+++ src/main/java/io/transpect/calabash/extensions/ImageIdentify.java
@@ -41,167 +41,161 @@ import com.xmlcalabash.runtime.XAtomicStep;
 
 public class ImageIdentify extends DefaultStep {
 	
-	private ReadablePipe mySource = null;
-    private WritablePipe myResult = null;
-    private WritablePipe myReport = null;
+  private ReadablePipe mySource = null;
+  private WritablePipe myResult = null;
+  private WritablePipe myReport = null;
     
-    private String formatDescription, mimeType, formatDetails, colorSpace, compressionAlgorithm, 
-		result="<c:error xmlns:c=\"http://www.w3.org/ns/xproc-step\">No file found</c:error>";
-    private int height;
-    private int width;
-    private int density;
-    private Boolean transparency = null;
+  private String formatDescription, mimeType, formatDetails, colorSpace, compressionAlgorithm,
+    result="<c:error xmlns:c=\"http://www.w3.org/ns/xproc-step\">No file found</c:error>";
+  private int height;
+  private int width;
+  private int density;
+  private Boolean transparency = null;
     
-    public ImageIdentify (XProcRuntime runtime, XAtomicStep step) {
-        super(runtime, step);
-    }
+  public ImageIdentify (XProcRuntime runtime, XAtomicStep step) {
+    super(runtime, step);
+  }
 
-    @Override
-    public void setInput (String port, ReadablePipe pipe) {
-        mySource = pipe;
-    }
+  @Override
+  public void setInput (String port, ReadablePipe pipe) {
+    mySource = pipe;
+  }
 
-    @Override
-    public void setOutput (String port, WritablePipe pipe) {
-    	if (port.equals("result"))
-    		myResult = pipe;
-    	if (port.equals("report"))
-    		myReport = pipe;
-    }
+  @Override
+  public void setOutput (String port, WritablePipe pipe) {
+    if (port.equals("result"))
+      myResult = pipe;
+    if (port.equals("report"))
+      myReport = pipe;
+  }
 
-    @Override
-    public void reset() {
-        mySource.resetReader();
-        myResult.resetWriter();
-        myReport.resetWriter();
-    }
+  @Override
+  public void reset() {
+    mySource.resetReader();
+    myResult.resetWriter();
+    myReport.resetWriter();
+  }
 
-	@Override
-    public void run() throws SaxonApiException {
-        super.run();
+  @Override
+  public void run() throws SaxonApiException {
+    super.run();
         
-    	RuntimeValue rValue = getOption(new QName("href"));
+    RuntimeValue rValue = getOption(new QName("href"));
 
-		while (mySource.moreDocuments()) {
-            XdmNode doc = mySource.read();
-            myResult.write(doc);
+    while (mySource.moreDocuments()) {
+      XdmNode doc = mySource.read();
+      myResult.write(doc);
+    }
+
+    try {
+      if (rValue != null) {
+        URI uri = rValue.getBaseURI().resolve(rValue.getString());
+        File file = new File(uri);
+        if (file.exists()) {
+          imageIdentify(file);
         }
+      }
 
-	    try {
-	    	if (rValue != null) {
-				URI uri = rValue.getBaseURI().resolve(rValue.getString());
-	    		File file = new File(uri);
-	    		if (file.exists()) {
-					imageIdentify(file);
-	    		}
-	    	}
+      if (result.startsWith("<c:results"))
+        result += "</c:results>";
 
-	    	if (result.startsWith("<c:results"))
-	    		result += "</c:results>";
+    } catch(Exception e) {
+      result = "<c:error xmlns:c=\"http://www.w3.org/ns/xproc-step\">" + e.getMessage() + "</c:error>";
+      e.printStackTrace();
+    }
 
-		} catch(Exception e) {
-			result = "<c:error xmlns:c=\"http://www.w3.org/ns/xproc-step\">" + e.getMessage() + "</c:error>";
-		    e.printStackTrace();
-		}
-
-	    DocumentBuilder builder = runtime.getProcessor().newDocumentBuilder();
-	    Source src = new StreamSource(new StringReader(result));
-	    XdmNode doc = builder.build(src);
+    DocumentBuilder builder = runtime.getProcessor().newDocumentBuilder();
+    Source src = new StreamSource(new StringReader(result));
+    XdmNode doc = builder.build(src);
 	    
-	    myReport.write(doc);
-    }
+    myReport.write(doc);
+  }
     
-    private void imageIdentify(File file) throws Exception {
-    	try {
-			org.apache.commons.imaging.ImageInfo imageInfo = Imaging.getImageInfo(file);
+  private void imageIdentify(File file) throws Exception {
+    try {
+      org.apache.commons.imaging.ImageInfo imageInfo = Imaging.getImageInfo(file);
 					
-			formatDescription = imageInfo.getFormatName();
-			mimeType = imageInfo.getMimeType();
-			formatDetails = imageInfo.getFormatDetails();
-			height = imageInfo.getHeight();
-			width = imageInfo.getWidth();
-			density = imageInfo.getPhysicalHeightDpi();
-			compressionAlgorithm = imageInfo.getCompressionAlgorithm();
-			transparency = imageInfo.isTransparent();
-			int color = imageInfo.getColorType();
-			if (color == 1){
-			    colorSpace = "GRAYSCALE";
-			} else {
-			    getColorSpace(file);
-			}
-		} catch (ImageReadException e) {
-			ImageManager imageManager = new ImageManager(new DefaultImageContext());
-			ImageSessionContext sessionContext = new DefaultImageSessionContext(imageManager.getImageContext(), null);
+      formatDescription = imageInfo.getFormatName();
+      mimeType = imageInfo.getMimeType();
+      formatDetails = imageInfo.getFormatDetails();
+      height = imageInfo.getHeight();
+      width = imageInfo.getWidth();
+      density = imageInfo.getPhysicalHeightDpi();
+      compressionAlgorithm = imageInfo.getCompressionAlgorithm().toString();
+      transparency = imageInfo.isTransparent();
+      colorSpace = imageInfo.getColorType().toString();
+    } catch (ImageReadException e) {
+      ImageManager imageManager = new ImageManager(new DefaultImageContext());
+      ImageSessionContext sessionContext = new DefaultImageSessionContext(imageManager.getImageContext(), null);
 
-			ImageInfo info = imageManager.getImageInfo(file.getPath(), sessionContext);
-			density = (int) info.getSize().getDpiHorizontal();
-			mimeType = info.getMimeType();
-			height = info.getSize().getHeightPx();
-			width = info.getSize().getWidthPx();
-		}
-    	
-    	result = "<c:results xmlns:c=\"http://www.w3.org/ns/xproc-step\" name=\""+file.getName()+"\">\n";
-    	getInfo();
+      ImageInfo info = imageManager.getImageInfo(file.getPath(), sessionContext);
+      density = (int) info.getSize().getDpiHorizontal();
+      mimeType = info.getMimeType();
+      height = info.getSize().getHeightPx();
+      width = info.getSize().getWidthPx();
     }
-    
-    private void getColorSpace(File file) throws Exception {
     	
-    	Integer colorSpaceType = null;
-    	
-		try {
-			BufferedImage image = Imaging.getBufferedImage(file);
-			colorSpaceType  = image.getColorModel().getColorSpace().getType();
-		} catch (ImageReadException er) {
-			try {
-				BufferedImage image = ImageIO.read(file);
-				colorSpaceType = image.getColorModel().getColorSpace().getType();
-			} catch (IIOException e) {
-				ICC_Profile iccProfile = Imaging.getICCProfile(file);
-		    	if (iccProfile != null) {
-		    		colorSpaceType = iccProfile.getColorSpaceType();
-		    	}
-			}
-		}
+    result = "<c:results xmlns:c=\"http://www.w3.org/ns/xproc-step\" name=\""+file.getName()+"\">\n";
+    getInfo();
+  }
+
+  // no longer used, to be removed soon
+  private void getColorSpace(File file) throws Exception {
+    Integer colorSpaceType = null;
+    try {
+      BufferedImage image = Imaging.getBufferedImage(file);
+      colorSpaceType  = image.getColorModel().getColorSpace().getType();
+    } catch (ImageReadException er) {
+      try {
+        BufferedImage image = ImageIO.read(file);
+        colorSpaceType = image.getColorModel().getColorSpace().getType();
+      } catch (IIOException e) {
+        ICC_Profile iccProfile = Imaging.getICCProfile(file);
+        if (iccProfile != null) {
+          colorSpaceType = iccProfile.getColorSpaceType();
+        }
+      }
+    }
 		
-		for (Field field : ColorSpace.class.getDeclaredFields()) {
-			if (field.getName().contains("TYPE") || field.getName().contains("CS")) {
-				int value = field.getInt(null);
-				if (colorSpaceType == value) {
-					String name = field.getName();
-					colorSpace = name.substring(name.indexOf("_")+1);
-				}
-			}
-		}
+    for (Field field : ColorSpace.class.getDeclaredFields()) {
+      if (field.getName().contains("TYPE") || field.getName().contains("CS")) {
+        int value = field.getInt(null);
+        if (colorSpaceType == value) {
+          String name = field.getName();
+          colorSpace = name.substring(name.indexOf("_")+1);
+        }
+      }
     }
+  }
     
-    private void getInfo() {
+  private void getInfo() {
     	    	
-		result += "\t<c:result name=\"mimetype\" value=\""+mimeType+"\"/>\n";
+    result += "\t<c:result name=\"mimetype\" value=\""+mimeType+"\"/>\n";
 		
-		if (formatDescription != null)
-			result += "\t<c:result name=\"formatdescription\" value=\""+formatDescription+"\"/>\n";
+    if (formatDescription != null)
+      result += "\t<c:result name=\"formatdescription\" value=\""+formatDescription+"\"/>\n";
 		
-		if (formatDetails != null)
-			result += "\t<c:result name=\"formatdetails\" value=\""+formatDetails+"\"/>\n";
+    if (formatDetails != null)
+      result += "\t<c:result name=\"formatdetails\" value=\""+formatDetails+"\"/>\n";
 		
-		result += "\t<c:result name=\"width\" value=\""+width+"px\"/>\n";
+    result += "\t<c:result name=\"width\" value=\""+width+"px\"/>\n";
 		
-		result += "\t<c:result name=\"height\" value=\""+height+"px\"/>\n";
+    result += "\t<c:result name=\"height\" value=\""+height+"px\"/>\n";
 		
-		if (density == -1) {
-			result += "\t<c:result name=\"density\" value=\"72dpi\"/>\n";
-		} else {
-			result += "\t<c:result name=\"density\" value=\""+density+"dpi\"/>\n";
-		}
-		
-		if (colorSpace != null)
-			result += "\t<c:result name=\"colorspace\" value=\""+colorSpace+"\"/>\n";
-		
-		if (transparency != null)
-			result += "\t<c:result name=\"transparency\" value=\""+transparency+"\"/>\n";
-		
-		if (compressionAlgorithm != null)
-			result += "\t<c:result name=\"compressionalgorithm\" value=\""+compressionAlgorithm+"\"/>\n";
-		
+    if (density == -1) {
+      result += "\t<c:result name=\"density\" value=\"72dpi\"/>\n";
+    } else {
+      result += "\t<c:result name=\"density\" value=\""+density+"dpi\"/>\n";
     }
+		
+    if (colorSpace != null)
+      result += "\t<c:result name=\"colorspace\" value=\""+colorSpace+"\"/>\n";
+		
+    if (transparency != null)
+      result += "\t<c:result name=\"transparency\" value=\""+transparency+"\"/>\n";
+		
+    if (compressionAlgorithm != null)
+      result += "\t<c:result name=\"compressionalgorithm\" value=\""+compressionAlgorithm+"\"/>\n";
+		
+  }
 }
